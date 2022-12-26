c37ebf63de7483c85f2118e39b232de691cf672a
Mike <shpoopdy@gmail.com>
2022-12-26 15:55:11 -0800

    Get familiar with C#

--- Obstacle Course/Assets/Mover.cs
+++ Obstacle Course/Assets/Mover.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+
+    float xValue = 0;
+    float yValue = 0.01f;
+    float zValue = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1, 0, 0);
+        transform.Translate(xValue, yValue, zValue);
     }
 }
