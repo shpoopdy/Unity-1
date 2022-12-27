56ac837ff7014e576c9a2fd3c785e1edac03ea54
Mike <shpoopdy@gmail.com>
2022-12-27 11:13:32 -0800

    Add method/refactor

--- Obstacle Course/Assets/Mover.cs
+++ Obstacle Course/Assets/Mover.cs
@@ -6,17 +6,24 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        PrintInstruction();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        MovePlayer();
+    }
+
+    void PrintInstruction() {
+        Debug.Log("Howdy hey.");
+        Debug.Log("To play use W,A,S,D.");
+        Debug.Log("No matter what, you smell.");
+    }
+
+    void MovePlayer() {
         float xValue = (Input.GetAxis("Horizontal") * Time.deltaTime) * moveSpeed;
         float zValue = (Input.GetAxis("Vertical") * Time.deltaTime) * moveSpeed;
-
         transform.Translate(xValue, 0, zValue);
     }
 }
